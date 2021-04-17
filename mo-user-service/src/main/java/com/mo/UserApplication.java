8611a6a8c70298e24714149ce7be116aff19430a
moweiquan <295597613@qq.com>
2021-04-17 20:56:18 +0800

    moshop-引入flyway数据库迁移工具，并创建数据库和moshop_user表

---
+++ mo-user-service/src/main/java/com/mo/UserApplication.java
@@ -0,0 +1,15 @@
+package com.mo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by mo on 2021/4/17
+ */
+@SpringBootApplication
+public class UserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class,args);
+    }
+}
