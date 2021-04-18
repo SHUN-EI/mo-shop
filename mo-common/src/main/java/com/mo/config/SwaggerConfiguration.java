c1210fc3f6c2450c412b5a876702a0f12513143e
moweiquan <295597613@qq.com>
2021-04-18 13:16:25 +0800

    moshop-SwaggerConfiguration 配置 接口文档分组，添加请求参数设置

--- mo-common/src/main/java/com/mo/config/SwaggerConfiguration.java
+++ mo-common/src/main/java/com/mo/config/SwaggerConfiguration.java
@@ -2,15 +2,17 @@ package com.mo.config;
 
 import lombok.Data;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.*;
 import springfox.documentation.oas.annotations.EnableOpenApi;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
+import springfox.documentation.schema.ScalarType;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by mo on 2021/4/18
@@ -20,11 +22,45 @@ import springfox.documentation.spring.web.plugins.Docket;
 @EnableOpenApi
 public class SwaggerConfiguration {
 
+    /**
+     * C端-用户相关
+     *
+     * @return
+     */
     @Bean
     public Docket webApiDoc() {
 
         return new Docket(DocumentationType.OAS_30)
                 .groupName("用户端接口文档")
+                .pathMapping("/")
+                //定义是否开启swagger，false为关闭，可以通过变量控制，线上关闭
+                .enable(true)
+                //配置api文档元信息
+                .apiInfo(apiInfo())
+                //选择哪些接口作为swagger的doc发布
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.mo"))
+                //正则匹配请求路径，并分配至当前分组
+                .paths(PathSelectors.ant("/api/**"))
+                //正则匹配请求路径，并分配至当前分组，当前所有接口
+                .paths(PathSelectors.any())
+                .build()
+                //新版swagger3.0配置
+                .globalRequestParameters(getGlobalRequestParameters())
+                .globalResponses(HttpMethod.GET,getGlobalResponseMessage())
+                .globalResponses(HttpMethod.POST,getGlobalResponseMessage());
+    }
+
+    /**
+     * 管理端
+     *
+     * @return
+     */
+    @Bean
+    public Docket adminApiDoc() {
+
+        return new Docket(DocumentationType.OAS_30)
+                .groupName("管理端接口文档")
                 .pathMapping("/")
                 // 定义是否开启swagger，false为关闭，可以通过变量控制，线上关闭
                 .enable(true)
@@ -36,6 +72,34 @@ public class SwaggerConfiguration {
                 //正则匹配请求路径，并分配至当前分组
                 .paths(PathSelectors.ant("/api/**"))
                 .build();
+    }
+
+
+    /**
+     * 生成全局通用参数, 支持配置多个响应参数
+     * @return
+     */
+    private List<RequestParameter> getGlobalRequestParameters() {
+        List<RequestParameter> parameters = new ArrayList<>();
+        parameters.add(new RequestParameterBuilder()
+                .name("token")
+                .description("登录令牌")
+                .in(ParameterType.HEADER)
+                .query(q -> q.model(m -> m.scalarModel(ScalarType.STRING)))
+                .required(false)
+                .build());
+
+        return parameters;
+    }
+
+    /**
+     * 生成通用响应信息
+     * @return
+     */
+    private List<Response> getGlobalResponseMessage() {
+        List<Response> responseList = new ArrayList<>();
+        responseList.add(new ResponseBuilder().code("4xx").description("请求错误，根据code和msg检查").build());
+        return responseList;
     }
 
     private ApiInfo apiInfo() {
