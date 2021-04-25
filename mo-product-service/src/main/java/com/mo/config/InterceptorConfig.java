eade626c148f75911aef1f273dc1f7fa5b6148a1
moweiquan <295597613@qq.com>
2021-04-25 13:54:38 +0800

    moshop-商品微服务，添加拦截器配置

---
+++ mo-product-service/src/main/java/com/mo/config/InterceptorConfig.java
@@ -0,0 +1,24 @@
+package com.mo.config;
+
+import com.mo.interceptor.LoginInterceptor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Created by mo on 2021/4/22
+ */
+@Configuration
+@Slf4j
+public class InterceptorConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginInterceptor())
+                //拦截路径
+                .addPathPatterns()
+                //不拦截路径
+                .excludePathPatterns();
+    }
+}
