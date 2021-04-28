ffe315ff20060a3f9279c325ddddb2d6c21643de
moweiquan <295597613@qq.com>
2021-04-28 20:32:12 +0800

    moshop-优惠券微服务，消息队列延迟消息功能测试用列编写

---
+++ mo-coupon-service/src/test/java/com/mo/biz/MQTest.java
@@ -0,0 +1,27 @@
+package com.mo.biz;
+
+import com.mo.CouponApplication;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Created by mo on 2021/4/28
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = CouponApplication.class)
+@Slf4j
+public class MQTest {
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    @Test
+    public void sendMessage() {
+        rabbitTemplate.convertAndSend("coupon.event.exchange","coupon.release.delay.routing.key","this is coupon record lock");
+    }
+}
