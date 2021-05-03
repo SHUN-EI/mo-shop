a95aa6dd130a421e23278aa8c93abd6760abfd7e
moweiquan <295597613@qq.com>
2021-05-03 12:53:05 +0800

    moshop-订单微服务-定时关单RabbitMQ延迟消息发送测试用例编写

--- mo-order-service/src/test/java/com/mo/biz/OrderTest.java
+++ mo-order-service/src/test/java/com/mo/biz/OrderTest.java
@@ -7,6 +7,7 @@ import com.mo.utils.OrderCodeGenerateUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -23,6 +24,13 @@ public class OrderTest {
 
     @Autowired
     private OrderCodeGenerateUtil orderCodeGenerateUtil;
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    @Test
+    public  void sendMessageTest(){
+        rabbitTemplate.convertAndSend("order.event.exchange","order.close.delay.routing.key","this is order close message");
+    }
 
     @Test
     public void OrderCodeGenerateTest() {
