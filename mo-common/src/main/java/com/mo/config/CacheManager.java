7fdc131a3cad7344666869c9723b2cb7da6bb176
moweiquan <295597613@qq.com>
2021-06-09 18:32:46 +0800

    moshop-SpringCache缓存框架引入

---
+++ mo-common/src/main/java/com/mo/config/CacheManager.java
@@ -0,0 +1,28 @@
+package com.mo.config;
+
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+import java.time.Duration;
+
+/**
+ * Created by mo on 2021/6/9
+ * 缓存管理类
+ */
+@Configuration
+@EnableCaching
+public class CacheManager extends CachingConfigurerSupport {
+
+    @Bean
+    public RedisCacheConfiguration redisCacheConfiguration() {
+        Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<>(Object.class);
+        RedisCacheConfiguration configuration = RedisCacheConfiguration.defaultCacheConfig();
+        configuration = configuration.serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(jackson2JsonRedisSerializer)).entryTtl(Duration.ofMinutes(1));
+        return configuration;
+    }
+}
